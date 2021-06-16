f6afe987c2deb4156e5143464cb7ac71a423f434
Lindson França <l1nds0n@hotmail.com>
2021-06-16 09:13:58 -0300

    Evento de update no clique duplo

--- HorseAPI/frontend/src/providers/Providers.Frames.Cliente.pas
+++ HorseAPI/frontend/src/providers/Providers.Frames.Cliente.pas
@@ -20,14 +20,15 @@ type
     procedure crlDeleteClick(Sender: TObject);
     procedure crlDeleteTap(Sender: TObject; const Point: TPointF);
     procedure crlEditTap(Sender: TObject; const Point: TPointF);
-  private
-    FId: string;
-    FOnDelete: TEventCallBack;
-    FOnUpdate: TEventCallBack;
-  public
-    property Id: string read FId write FId;
-    property OnDelete: TEventCallBack read FOnDelete write FOnDelete;
-    property OnUpdate: TEventCallBack read FOnUpdate write FOnUpdate;
+    procedure retContentDblClick(Sender: TObject);
+    private
+      FId: string;
+      FOnDelete: TEventCallBack;
+      FOnUpdate: TEventCallBack;
+    public
+      property Id: string read FId write FId;
+      property OnDelete: TEventCallBack read FOnDelete write FOnDelete;
+      property OnUpdate: TEventCallBack read FOnUpdate write FOnUpdate;
   end;
 
 implementation
@@ -35,35 +36,43 @@ implementation
 {$R *.fmx}
 
 procedure TFrameCliente.crlDeleteClick(Sender: TObject);
-begin
-{$IFDEF MSWINDOWS}
+  begin
+  {$IFDEF MSWINDOWS}
   if Assigned(FOnDelete) then
     FOnDelete(Self, FId);
-{$ENDIF}
-end;
+  {$ENDIF}
+  end;
 
 procedure TFrameCliente.crlDeleteTap(Sender: TObject; const Point: TPointF);
-begin
-{$IFNDEF MSWINDOWS}
+  begin
+  {$IFNDEF MSWINDOWS}
   if Assigned(FOnDelete) then
     FOnDelete(Self, FId);
-{$ENDIF}
-end;
+  {$ENDIF}
+  end;
 
 procedure TFrameCliente.crlEditClick(Sender: TObject);
-begin
-{$IFDEF MSWINDOWS}
+  begin
+  {$IFDEF MSWINDOWS}
   if Assigned(FOnUpdate) then
     FOnUpdate(Self, FId);
-{$ENDIF}
-end;
+  {$ENDIF}
+  end;
 
 procedure TFrameCliente.crlEditTap(Sender: TObject; const Point: TPointF);
-begin
-{$IFNDEF MSWINDOWS}
+  begin
+  {$IFNDEF MSWINDOWS}
   if Assigned(FOnUpdate) then
     FOnUpdate(Self, FId);
-{$ENDIF}
-end;
+  {$ENDIF}
+  end;
+
+procedure TFrameCliente.retContentDblClick(Sender: TObject);
+  begin
+  {$IFDEF MSWINDOWS}
+  if Assigned(FOnUpdate) then
+    FOnUpdate(Self, FId);
+  {$ENDIF}
+  end;
 
 end.
